6a5915ccfa85c44b82e62188d75ac43dfa54ee01
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 21:17:15 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def collect(array)
+  if block_given?
+    i = 0 
+      collection = []
+        while i < array.length 
+        collection << yield(array[i])
+          i += 1 
+        end
+  else 
+    "please provide block"
+  end
